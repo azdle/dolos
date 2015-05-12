0ac89401cd76243f6f3683f15d0dfce71e8e42b0
Patrick Barrett <pat@theunlink.com>
2015-05-12 15:24:36 -0500

    support unlimited* clients

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use std::net::UdpSocket;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, SocketAddrV4};
 use std::thread;
 use core::str::FromStr;
+use std::collections::hash_map::HashMap;
 
 
 static USAGE: &'static str =    "
@@ -46,6 +47,13 @@ fn spawn_return_socket_thread(local_socket: UdpSocket, remote_socket: UdpSocket,
     });
 }
 
+fn print_u8(buf: &[u8]){
+    for i in buf.iter() {
+        print!("0x{:0>2X}, ", i)
+    }
+    println!("")
+}
+
 fn main() {
     let args: Args = Docopt::new(USAGE)
                             .and_then(|d| d.decode())
@@ -59,33 +67,34 @@ fn main() {
     let remote_addr = SocketAddrV4::new(Ipv4Addr::new(0,0,0,0), 0);
     let dest_addr = SocketAddr::new(IpAddr::from_str(args.arg_dstip.as_str()).ok().expect("couldn't parse dest ip"), args.arg_dstport);
 
+    // Socket for incoming requests from proxy clients
     let local_socket = match UdpSocket::bind(local_addr) {
         Ok(s) => s,
         Err(e) => panic!("couldn't bind local socket: {}", e),
     };
 
-    let remote_socket = match UdpSocket::bind(remote_addr) {
-        Ok(s) => s,
-        Err(e) => panic!("couldn't bind remote socket: {}", e),
-    };
-
     let mut buf = [0; 2048];
-    let mut proxy_src: Option<SocketAddr> = None;
+    let mut src_to_socket_list: HashMap<SocketAddr, UdpSocket> = HashMap::new();
+
     loop {
         match local_socket.recv_from(&mut buf) {
             Ok((amt, src)) => {
-                if proxy_src.is_some() && src != proxy_src.unwrap() {
-                    println!("WARNING: Received from new src while already bound, dropping.");
-                    return
-                } else if proxy_src.is_none() {
+                let remote_socket = src_to_socket_list.entry(src).or_insert_with(|| {
+                    println!("new client: {}", src);
+
+                    // socket for server being proxied to
+                    let remote_socket =  UdpSocket::bind(remote_addr).ok().expect("couldn't bind remote socket");
+
                     spawn_return_socket_thread(
                         local_socket.try_clone().ok().expect("couldn't clone lsock"),
                         remote_socket.try_clone().ok().expect("couldn't clone rsock"),
-                        proxy_src.unwrap());
+                        src);
 
-                    proxy_src = Some(src);
-                }
-                // Send a reply to the socket we received data from
+                    remote_socket
+                });
+
+
+                // send message to dest server
                 let buf = &mut buf[.. amt];
                 remote_socket.send_to(buf, dest_addr).ok();
                 print!(" >--> ");
@@ -94,11 +103,4 @@ fn main() {
             Err(e) => println!("couldn't receive a datagram: {}", e)
         }
     }
-}
-
-fn print_u8(buf: &[u8]){
-    for i in buf.iter() {
-        print!("0x{:0>2X}, ", i)
-    }
-    println!("")
 }
